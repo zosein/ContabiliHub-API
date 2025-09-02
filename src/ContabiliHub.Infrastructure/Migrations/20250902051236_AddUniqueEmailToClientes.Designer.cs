dac0aef856ad47ebf16bbaed653a6cc120325928
zosein <italoledz@gmail.com>
2025-09-02 04:51:34 -0400

    feat: add novas migrations de validação de email único por cliente

--- src/ContabiliHub.Infrastructure/Migrations/20250902051236_AddUniqueEmailToClientes.Designer.cs
+++ src/ContabiliHub.Infrastructure/Migrations/20250902051236_AddUniqueEmailToClientes.Designer.cs
@@ -4,6 +4,7 @@ using ContabiliHub.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContabiliHub.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250902051236_AddUniqueEmailToClientes")]
+    partial class AddUniqueEmailToClientes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ContabiliHub.Infrastructure/Migrations/20250902051236_AddUniqueEmailToClientes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContabiliHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueEmailToClientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_Email",
+                table: "Clientes",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Clientes_Email",
+                table: "Clientes");
+        }
+    }
+}
